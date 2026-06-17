e8790d5aa896884f4750f2276edae3626a69aa6f
Karis-ui <kariukic547@gmail.com>
2026-06-17 23:19:17 +0300

    fix: updated files

--- Backend/Hospital/settings.py
+++ Backend/Hospital/settings.py
@@ -94,8 +94,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Hospital.urls'
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127`.0.0.1:3000',
+    smartcare.up.railway.app
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_CREDENTIALS = True
