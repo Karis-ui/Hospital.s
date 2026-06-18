38c1adee405b56df494d083e90ea721d47cb19de
Karis-ui <kariukic547@gmail.com>
2026-06-18 14:56:27 +0300

    fix: update corrupted files

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -281,7 +281,7 @@ class Activity(models.Model):
 class AdminAnnouncement(models.Model):
     title = models.CharField(max_length=100)
     message = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"{self.title} {self.message}"
