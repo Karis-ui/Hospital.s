4542ecb5ed7ecf844ea0476743a475289f9d864f
Karis-ui <kariukic547@gmail.com>
2026-06-18 13:34:46 +0300

    fix: all

--- Backend/Hospital/urls.py
+++ Backend/Hospital/urls.py
@@ -21,7 +21,7 @@ from rest_framework_simplejwt.views import(
     TokenObtainPairView,TokenRefreshView
 )
 urlpatterns = [
-    path('',RedirectView.as_view(url='/home/',permanent=False)),
+    path('',RedirectView.as_view(url='/home/',permanent=False,name='root')),
     path('admin/', admin.site.urls),
     path('api/token/',TokenObtainPairView.as_view()),
     path('api/token/refresh/',TokenRefreshView.as_view()),
