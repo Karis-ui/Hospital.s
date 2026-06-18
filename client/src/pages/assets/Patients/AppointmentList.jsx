75f75af6330a7ab448de554826fc5e0c0c030c63
Karis-ui <kariukic547@gmail.com>
2026-06-18 07:44:28 +0300

    fix: updated files

--- client/src/pages/assets/Patients/AppointmentList.jsx
+++ client/src/pages/assets/Patients/AppointmentList.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useConfirm } from 'react';
+import React, { useState, useEffect } from 'react';
+import {useConfirm} from '../../../theme/useConfirm';
 import { useNavigate } from 'react-router-dom';
 import {
   Box,
