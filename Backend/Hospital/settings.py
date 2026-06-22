7c2128f120ba1507147603dbe9b77de6afd1d262
Karis-ui <kariukic547@gmail.com>
2026-06-22 15:37:15 +0300

    fix: update all files

--- Backend/Hospital/settings.py
+++ Backend/Hospital/settings.py
@@ -150,7 +150,7 @@ if DATABASE_URL:
 else:
     DATABASES = {
         'default':{
-            'ENGINE':'django.db.backend.sqlite3',
+            'ENGINE':'django.db.backends.sqlite3',
             'NAME':BASE_DIR / 'db.sqlite3',
         }
     }
