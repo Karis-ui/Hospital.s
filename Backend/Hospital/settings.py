ce9bb6c076d9094bba2e3e0da97e1bcda5b52ddd
Karis-ui <kariukic547@gmail.com>
2026-07-04 19:47:30 +0300

    fix: update settings file

--- Backend/Hospital/settings.py
+++ Backend/Hospital/settings.py
@@ -97,16 +97,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES':[
         'rest_framework.permissions.IsAuthenticated',
     ],
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.UserRateThrottle',
-        'rest_framework.throttling.AnonRateThrottle',
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'user': '1000/day',
-        'anon': '100/day',
-        'login': '10/hour',
-        'register': '5/hour',
-    },
 }
 
 MIDDLEWARE = [
